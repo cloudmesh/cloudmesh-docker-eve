4ca23e5cad6e3bf61082b645b22ccb072fa4719e
kpvenkat <vkarthickprabu@gmail.com>
2017-03-15 17:03:41 -0600

    new:usr: Intial Commit include extbar

---
+++ cloudmesh/ext/command/bar.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+from cloudmesh_client.shell.command import command
+from cloudmesh_client.shell.command import PluginCommand
+
+
+class BarCommand(PluginCommand):
+
+    @command
+    def do_bar(self, args, arguments):
+        """
+        ::
+
+          Usage:
+                command -f FILE
+                command FILE
+                command list
+
+          This command does some useful things.
+
+          Arguments:
+              FILE   a file name
+
+          Options:
+              -f      specify the file
+
+        """
+        print(arguments)
+
+
+
